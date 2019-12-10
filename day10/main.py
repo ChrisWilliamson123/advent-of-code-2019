e8c3068e5b74c1e30a1178f3b1a7f765671606de
Chris Williamson <chris.williamson@bbc.co.uk>
2019-12-10 13:01:49 +0000

    Completed day 10

--- day10/main.py
+++ day10/main.py
@@ -1,40 +1,63 @@
 from math import sqrt, atan, degrees
 
-input_rows = [list(l.rstrip()) for l in open('input.txt', 'r').readlines()]
-# input_rows = [list(l.rstrip()) for l in open('test_input.txt', 'r').readlines()]
+def get_asteroid_coords(input_rows):
+  asteroid_coords = set()
+  for y in range(len(input_rows)):
+    for x in range(len(input_rows[y])):
+      if input_rows[y][x] == '#':
+        asteroid_coords.add((x, y))
+  return asteroid_coords
 
-asteroid_coords = set()
-for y in range(len(input_rows)):
-  for x in range(len(input_rows[y])):
-    if input_rows[y][x] == '#':
-      asteroid_coords.add((x, y))
-
-viewable_neighbours = []
-for asteroid in asteroid_coords:
-  surrounding = asteroid_coords - {asteroid}
-  angles = set()
+# Returns a list of neighbours in the format [neighbour_coord, distance, angle]
+def get_neighbours(home_coord, asteroid_coords):
+  surrounding = asteroid_coords - {home_coord}
+  neighbours = []
   for neighbour in surrounding:
-    x_difference = float(neighbour[0] - asteroid[0])
-    y_difference = float(neighbour[1] - asteroid[1])
+    x_difference = float(neighbour[0] - home_coord[0])
+    y_difference = float(neighbour[1] - home_coord[1])
+    distance = sqrt(x_difference**2 + y_difference**2)
 
     if x_difference == 0:
-      if y_difference > 0:
-        angle = 180
-      else:
-        angle = 0
+      angle = 180 if y_difference > 0 else 0
     elif y_difference == 0:
-      if x_difference > 0:
-        angle = 90
-      else:
-        angle = 270
+      angle = 90 if x_difference > 0 else 270
     else:
-      angle = degrees(atan(float(y_difference)/float(x_difference))) * -1
+      angle = degrees(atan(x_difference/y_difference)) * -1
       
       if y_difference > 0:
         angle += 180
-    
-    angles.add(angle)
 
-  viewable_neighbours.append(len(angles))
+      if angle < 0:
+        angle += 360
+    if home_coord == (8, 3):
+      print(neighbour, angle)
+    neighbours.append([neighbour, distance, angle])
+  return neighbours
 
-print(max(viewable_neighbours))
+input_rows = [list(l.rstrip()) for l in open('input.txt', 'r').readlines()]
+
+asteroid_coords = get_asteroid_coords(input_rows)
+
+amount_of_viewable_neighbours = []
+for asteroid in asteroid_coords:
+  neighbours = get_neighbours(asteroid, asteroid_coords)
+  amount_of_viewable_neighbours.append((asteroid, len(set(map(lambda n: n[2], neighbours)))))
+
+best_option = max(amount_of_viewable_neighbours, key=lambda x: x[1])
+print(best_option[1])
+
+base_station = best_option[0]
+
+neighbours = get_neighbours(base_station, asteroid_coords)
+
+destroyed = []
+while len(neighbours) > 0:
+  angles = list(set(map(lambda n: n[2], neighbours)))
+  angles.sort()
+
+  for angle in angles:
+    closest_neighbour_at_angle = min(filter(lambda n: n[2] == angle, neighbours), key=lambda n: n[1])
+    neighbours.remove(closest_neighbour_at_angle)
+    destroyed.append(closest_neighbour_at_angle)
+
+print(destroyed[199][0][0]*100 + destroyed[199][0][1])
