507819d5ce27d1b29fd82f987b9bb5d203898a18
Chris Williamson <chris.williamson@bbc.co.uk>
2019-12-13 15:38:05 +0000

    Using new intcode computer function

--- day5/main.py
+++ day5/main.py
@@ -7,12 +7,10 @@ intcode = [int(x) for x in open('input.txt', 'r').read().split(',')]
 
 # Part one
 computer = IntcodeComputer(intcode[:], [1])
-while not computer.halted:
-  computer.perform_next_operation()
+computer.run_until_halted()
 print(computer.outputs[-1])
 
 # Part two
 computer = IntcodeComputer(intcode[:], [5])
-while not computer.halted:
-  computer.perform_next_operation()
+computer.run_until_halted()
 print(computer.outputs[0])
--- day9/main.py
+++ day9/main.py
@@ -4,12 +4,10 @@ sys.path.append('..')
 from intcode_computer import IntcodeComputer
 
 intcode = [int(x) for x in open('input.txt', 'r').read().split(',')]
-part_one_computer = IntcodeComputer(intcode[:], [1])
-while not part_one_computer.halted:
-  part_one_computer.perform_next_operation()
-print(part_one_computer.outputs[0])
+computer = IntcodeComputer(intcode[:], [1])
+computer.run_until_halted()
+print(computer.outputs[0])
 
-part_two_computer = IntcodeComputer(intcode[:], [2])
-while not part_two_computer.halted:
-  part_two_computer.perform_next_operation()
-print(part_two_computer.outputs[0])
+computer = IntcodeComputer(intcode[:], [2])
+computer.run_until_halted()
+print(computer.outputs[0])
