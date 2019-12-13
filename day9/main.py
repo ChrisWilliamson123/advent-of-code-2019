87fbdae975465e5c25f9a504f50df565f3d9f1a7
Chris Williamson <chris.williamson@bbc.co.uk>
2019-12-13 14:58:57 +0000

    Changed day9 to use new intcode computer

--- day9/main.py
+++ day9/main.py
@@ -1,11 +1,15 @@
+import sys
+sys.path.append('..')
+
 from intcode_computer import IntcodeComputer
 
-intcode = open('input.txt', 'r').read().split(',')
-intcode = map(int, intcode)
+intcode = [int(x) for x in open('input.txt', 'r').read().split(',')]
 part_one_computer = IntcodeComputer(intcode[:], [1])
-part_one_computer.run_program()
+while not part_one_computer.halted:
+  part_one_computer.perform_next_operation()
 print(part_one_computer.outputs[0])
-part_two_computer = IntcodeComputer(intcode[:], [2])
-part_two_computer.run_program()
-print(part_two_computer.outputs[0])
 
+part_two_computer = IntcodeComputer(intcode[:], [2])
+while not part_two_computer.halted:
+  part_two_computer.perform_next_operation()
+print(part_two_computer.outputs[0])
