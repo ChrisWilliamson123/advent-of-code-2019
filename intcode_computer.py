d7f7074655caa0b48dcc7ec70bc971cff9723b3d
Chris Williamson <chris.williamson@bbc.co.uk>
2019-12-13 14:16:02 +0000

    Final bits

--- intcode_computer.py
+++ intcode_computer.py
@@ -15,6 +15,36 @@ class IntcodeComputer:
     
     self.halted = False
 
+  def perform_next_operation(self):
+    (opcode, modes) = self.parse_operation_spec(self.memory[self.ip])
+    
+    if opcode == 99:
+      self.halted = True
+      return
+
+    addresses = self.get_addresses_from_parameter_modes(modes)
+
+    if opcode == 1:
+      self.op_add(addresses[0], addresses[1], addresses[2])
+    elif opcode == 2:
+      self.op_mul(addresses[0], addresses[1], addresses[2])
+    elif opcode == 3:
+      self.op_input(addresses[0])
+    elif opcode == 4:
+      self.op_output(addresses[0])
+    elif opcode == 5:
+      self.op_jit(addresses[0], addresses[1])
+    elif opcode == 6:
+      self.op_jif(addresses[0], addresses[1])
+    elif opcode == 7:
+      self.op_lt(addresses[0], addresses[1], addresses[2])
+    elif opcode == 8:
+      self.op_eq(addresses[0], addresses[1], addresses[2])
+    elif opcode == 9:
+      self.op_change_relative_base(addresses[0])
+
+    self.ip += self.skip_ahead_amount(opcode)
+
   def parse_operation_spec(self, spec):
     opcode = int(str(spec)[-2:]) 
     param_modes =  [(spec//100)%10, (spec//1000)%10, (spec//10000)%10]
@@ -22,49 +52,18 @@ class IntcodeComputer:
 
   def get_addresses_from_parameter_modes(self, modes):
     addresses = []
-    for i in range(1, len(modes)+1):
-      parameter_location = self.ip + i
+    for i in range(len(modes)):
+      parameter_location = self.ip + i + 1
       # RELATIVE
-      if modes[i-1] == 2:
+      if modes[i] == 2:
         addresses.append(self.relative_base + self.memory[parameter_location])
       # IMMEDIATE
-      elif modes[i-1] == 1:
+      elif modes[i] == 1:
         addresses.append(parameter_location)
       # POSTION
       else:
         addresses.append(self.memory[parameter_location])
     return addresses
-
-  # Returns: list [value1, value2]
-  # Will only return two values if operation needs both
-  def get_param_values(self, opcode, modes):
-    values = []
-    for i in range(1, len(modes)+1):
-      if modes[i-1] == 2:
-        values.append(self.memory[self.relative_base + self.memory[self.ip+i]])
-      elif modes[i-1] == 1:
-        values.append(self.memory[self.ip+i])
-      else:
-        values.append(self.memory[self.memory[self.ip+i]])
-    return values
-
-  def skip_ahead_amount(self, opcode):
-    return {
-      1: 4,
-      2: 4,
-      3: 2,
-      4: 2,
-      5: 0,
-      6: 0,
-      7: 4,
-      8: 4,
-      9: 2
-    }[opcode]
-
-  def get_result_addr(self, mode, value):
-    if mode == 2:
-      return self.relative_base + value
-    return value
 
   def op_add(self, addr1, addr2, result_addr):
     self.memory[result_addr] = self.memory[addr1] + self.memory[addr2]
@@ -90,41 +89,18 @@ class IntcodeComputer:
   def op_eq(self, addr1, addr2, result_addr):
     self.memory[result_addr] = 1 if self.memory[addr1] == self.memory[addr2] else 0
 
-  def perform_next_operation(self):
-    # Get the operation specification
-    (opcode, [param1_mode, param2_mode, param3_mode]) = self.parse_operation_spec(self.memory[self.ip])
-    
-    if opcode == 99:
-      self.halted = True
-      return
-      
-    values = self.get_param_values(opcode, [param1_mode, param2_mode, param3_mode])
-    addresses = self.get_addresses_from_parameter_modes([param1_mode, param2_mode, param3_mode])
+  def op_change_relative_base(self, addr1):
+    self.relative_base += self.memory[addr1]
 
-    if opcode == 1:
-      self.op_add(addresses[0], addresses[1], addresses[2])
-    elif opcode == 2:
-      self.op_mul(addresses[0], addresses[1], addresses[2])
-    elif opcode == 3:
-      self.op_input(addresses[0])
-    elif opcode == 4:
-      self.op_output(addresses[0])
-    elif opcode == 5:
-      self.op_jit(addresses[0], addresses[1])
-    elif opcode == 6:
-      self.op_jif(addresses[0], addresses[1])
-    elif opcode == 7:
-      self.op_lt(addresses[0], addresses[1], addresses[2])
-    elif opcode == 8:
-      self.op_eq(addresses[0], addresses[1], addresses[2])
-    elif opcode == 9:
-      param = self.memory[self.ip+1]
-      if param1_mode == 2:
-        change = self.memory[self.relative_base + param]
-      elif param1_mode == 1:
-        change = param
-      else:
-        change = self.memory[param]
-      self.relative_base += change
-
-    self.ip += self.skip_ahead_amount(opcode)
+  def skip_ahead_amount(self, opcode):
+    return {
+      1: 4,
+      2: 4,
+      3: 2,
+      4: 2,
+      5: 0,
+      6: 0,
+      7: 4,
+      8: 4,
+      9: 2
+    }[opcode]
