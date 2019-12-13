d74efd3b661bc37a0e798fe17cbdc2c5f255fe94
Chris Williamson <chris.williamson@bbc.co.uk>
2019-12-13 15:26:43 +0000

    Converted day5 to use the new intcode computer

--- day5/main.py
+++ day5/main.py
@@ -1,13 +1,18 @@
+import sys
+sys.path.append('..')
+
 from intcode_computer import IntcodeComputer
 
 intcode = [int(x) for x in open('input.txt', 'r').read().split(',')]
 
 # Part one
 computer = IntcodeComputer(intcode[:], [1])
-computer.run_program()
-print(computer.output)
+while not computer.halted:
+  computer.perform_next_operation()
+print(computer.outputs[-1])
 
 # Part two
 computer = IntcodeComputer(intcode[:], [5])
-computer.run_program()
-print(computer.output)
+while not computer.halted:
+  computer.perform_next_operation()
+print(computer.outputs[0])
