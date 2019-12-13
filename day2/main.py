0f39732eeda9b1ab68c688dd7c32a7f21c42ecf1
Chris Williamson <chris.williamson@bbc.co.uk>
2019-12-13 15:34:52 +0000

    Converted day2 to use new intcode computer

--- day2/main.py
+++ day2/main.py
@@ -1,42 +1,28 @@
+import sys
+sys.path.append('..')
+
+from intcode_computer import IntcodeComputer
+
 intcode = [int(x) for x in open('input.txt', 'r').read().split(',')]
 
-def insert_inputs(intcode, noun, verb):
-  intcode[1] = noun 
-  intcode[2] = verb
-  return intcode
+part_one_intcode = intcode[:]
+part_one_intcode[1:3] = [12, 2]
+computer = IntcodeComputer(part_one_intcode, [])
 
-def add(intcode, pos1, pos2, resPos):
-    intcode[resPos]=intcode[pos1]+intcode[pos2]
-    
-def mul(intcode, pos1, pos2, resPos):
-    intcode[resPos]=intcode[pos1]*intcode[pos2]
-
-def run_intcode(intcode, noun, verb):
-  intcode = insert_inputs(intcode, noun, verb)
-
-  index = 0
-  operation = intcode[index]
-
-  while operation != 99:
-    if operation == 1:
-        add(intcode, intcode[index+1], intcode[index+2], intcode[index+3])
-    elif operation == 2:
-        mul(intcode, intcode[index+1], intcode[index+2], intcode[index+3])
-        
-    index+=4
-    operation=intcode[index] 
-
-  return intcode[0]
-
-part_one_input = intcode[:]
-part_one = run_intcode(part_one_input, 12, 2)
-print(part_one)
+while not computer.halted:
+  computer.perform_next_operation()
+print(computer.memory[0])
 
 for noun in range(100):
   for verb in range(100):
     ic = intcode[:]
-    output = run_intcode(ic, noun, verb)
+    ic[1:3] = [noun, verb]
+    computer = IntcodeComputer(ic, [])
 
+    while not computer.halted:
+      computer.perform_next_operation()
+    output = computer.memory[0]
+    
     if output == 19690720:
       print(100 * noun + verb)
       exit(0)
