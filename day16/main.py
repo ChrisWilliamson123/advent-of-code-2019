2fc8ac472653358a04fca5aa5de3e4b95f598fc7
Chris Williamson <chris.williamson@bbc.co.uk>
2019-12-16 15:15:07 +0000

    Refactoring

--- day16/main.py
+++ day16/main.py
@@ -1,23 +1,18 @@
-from itertools import islice, chain, cycle
-
 input = [int(x) for x in open('input.txt', 'r').read()]
 
-repeating_pattern_base = [0, 1, 0, -1]
-current_pattern = (input[:] * 10000)
+current_pattern = (input * 10000)
 
 phases = 100
-offset = 5970221
+offset = int(''.join(map(str,input[0:7])))
 
-current_pattern_length = len(current_pattern)
-half_pattern_length = current_pattern_length / 2
+pattern_length = len(current_pattern)
 
 for i in range(phases):
   total = 0
   output = []
-  for j in range(current_pattern_length, offset, -1):
-    total += current_pattern[j-1]
-    total %= 10
+  for j in range(pattern_length, offset, -1):
+    total = (total + current_pattern[j-1]) % 10
     output.append(total)
-  current_pattern = current_pattern[0:offset] + output[::-1]
+  current_pattern = current_pattern[:offset] + output[::-1]
 
 print(''.join([str(x) for x in current_pattern[offset:offset+8]]))
